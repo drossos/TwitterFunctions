44a3eb31490db1321bac0005da7a66930289feb8
drossos <daniel.rossos@mail.utoronto.ca>
2019-07-20 22:57:27 -0400

    seperate auth file

---
+++ tweet_auth_creds.py
@@ -0,0 +1,6 @@
+#Strings auth for using tweepy api
+
+consumer_key = 'DSKPgaFUZ7q7FVgcrJp3FYEvk'
+consumer_secret = 'nMfxwQ44EUwf5UPWTPUZZK83hkxDzDKfS30as2Ipen4AgpMi4k'
+access_token = '1131008741736951809-tRDxdJn8XtoevbDgNqagL2W3P66wNU'
+access_secret = 'k66MriO3EsDKLeZrdbMpdwrdH5TOpNmosOYCxJEhUuRMU'
--- tweet_fnts.py
+++ tweet_fnts.py
@@ -1,15 +1,12 @@
 import tweepy
 import json
 import datetime
-
+import tweet_auth_creds as tac
 
 #initialization
-consumer_key = 'DSKPgaFUZ7q7FVgcrJp3FYEvk'
-consumer_secret = 'nMfxwQ44EUwf5UPWTPUZZK83hkxDzDKfS30as2Ipen4AgpMi4k'
-access_token = '1131008741736951809-tRDxdJn8XtoevbDgNqagL2W3P66wNU'
-access_secret = 'k66MriO3EsDKLeZrdbMpdwrdH5TOpNmosOYCxJEhUuRMU'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+
+auth = tweepy.OAuthHandler(tac.consumer_key, tac.consumer_secret)
+auth.set_access_token(tac.access_token, tac.access_secret)
 
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
